c04088db0a744dcc453a3c0916852a0ca46ede24
Ali Havasi <alihvs@gmail.com>
2018-08-19 00:28:47 +0430

    saving master

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,15 +11,15 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Lato:400,700'
-      }
-    ]
+        href: 'https://fonts.googleapis.com/css?family=Lato:400,700',
+      },
+    ],
   },
 
   /*
@@ -51,6 +51,6 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {}
-  }
+    extend(config, ctx) {},
+  },
 };
